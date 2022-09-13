6e40d6e1bade950288b477dabf9ca24337cdc0a2
nedimmujcinovic <nedimmujcinovic1@gmail.com>
2022-09-13 21:06:03 +0200

    I added some functions in js file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('form');
 const container = document.getElementsByClassName('container');
 const searchBar = document.getElementsByClassName('searchbar')[0];
-const submitBtn = document.getElementsByClassName('submitBtn');
+const submitBtn = document.getElementsByClassName('submitBtn')[0];
 const displayContainer = document.getElementsByClassName('displayContainer');
 const temperature = document.getElementsByClassName('temperature');
 const description = document.getElementsByClassName('description');
@@ -9,14 +9,24 @@ const humidity = document.getElementsByClassName('humidity');
 const feelsLike = document.getElementsByClassName('feelsLike');
 const api = '0b8981366e5ef28551fb2db8fa544438';
 
-form.addEventListener("submit", (e) => {
+function fetchApi(city){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api}`)
+    .then((response) => response.json())
+    .then((data) => displayWeather(data));
+}
 
-    if (searchBar.value === "") {
-        alert("Input is empty");
+submitBtn.addEventListener("click", (e) => {
+    if (searchBar.value != "") {
+        fetchApi(searchBar.value);
     }
-    else
-        alert("input is valid:  " + searchBar.value);
-
+    else console.log("not works");
 }
 );
+
+function displayWeather(data){
+    container.style.display = 'none';
+    displayContainer.style.display = 'flex';
+    
+} 
+
 
